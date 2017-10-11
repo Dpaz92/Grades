7ebee9671cfedf15d5395ca2c0ea8186791d2476
Dpaz92 <32526541+Dpaz92@users.noreply.github.com>
2017-10-11 11:54:01 -0300

    2

--- Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -11,11 +11,22 @@ namespace Grades.Tests.Types
     public class ReferenceTypeTests
     {
         [TestMethod]
-        public void VariablesHoldReference()
+        public void IntVariablesHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        [TestMethod]
+        public void GradeBookVariablesHoldReference()
         {
             Gradebook g1 = new Gradebook();
             Gradebook g2 = g1;
 
+            g1 = new Gradebook();
             g1.Name = "Scott's grade book";
             Assert.AreEqual(g1.Name, g2.Name);
         }
